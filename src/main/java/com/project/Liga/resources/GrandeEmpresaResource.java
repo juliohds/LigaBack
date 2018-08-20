619102c821eb8bf65023b94fa42dbd0d0bbd0d91
Julio Henrique <tecjuliohenrique@gmail.com>
2018-08-20 04:32:32 -0300

    endpoints grande empresa * alllow

--- src/main/java/com/project/Liga/resources/GrandeEmpresaResource.java
+++ src/main/java/com/project/Liga/resources/GrandeEmpresaResource.java
@@ -1,8 +1,11 @@
 package com.project.Liga.resources;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,15 +14,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.Liga.models.Endereco;
 import com.project.Liga.models.GrandeEmpresa;
+import com.project.Liga.models.Socio;
+import com.project.Liga.repository.EnderecoRepository;
 import com.project.Liga.repository.GrandeEmpresaRepository;
+import com.project.Liga.repository.SocioRepository;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/grande-empresa")
 public class GrandeEmpresaResource {
 	
 	@Autowired
 	private GrandeEmpresaRepository gr;
+	@Autowired 
+	private SocioRepository sor;
+	@Autowired 
+	private EnderecoRepository er;
 	
 	@GetMapping(produces="application/json")
 	public @ResponseBody Iterable<GrandeEmpresa> listarGrandeEmpresa() {
@@ -29,6 +41,23 @@ public class GrandeEmpresaResource {
 	
 	@PostMapping()
 	public @Valid GrandeEmpresa cadastraGrandeEmpresa(@RequestBody @Valid GrandeEmpresa grandeEmpresa) {
+		
+		if(grandeEmpresa.getEndereco() != null) {
+			Endereco end = grandeEmpresa.getEndereco();
+			er.save(end);
+		}
+
+		if(grandeEmpresa.getSocio() != null) {
+			
+			List<Socio> ls = grandeEmpresa.getSocio();
+		
+		if(!ls.isEmpty()) {
+			for (Socio c : ls) {
+				sor.save(c);
+			}
+		}
+		}
+		
 		return gr.save(grandeEmpresa);
 	}
 	
--- src/main/java/com/project/Liga/resources/SocioResource.java
+++ src/main/java/com/project/Liga/resources/SocioResource.java
@@ -3,6 +3,7 @@ package com.project.Liga.resources;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.project.Liga.models.Socio;
 import com.project.Liga.repository.SocioRepository;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/socio")
 public class SocioResource {
