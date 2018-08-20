03754d3e7e4bbcca32a2bc258fbecfa30e1ba59d
Julio Henrique <tecjuliohenrique@gmail.com>
2018-08-20 10:19:10 -0300

    alter run time java

--- src/main/java/com/project/Liga/LigaApplication.java
+++ src/main/java/com/project/Liga/LigaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LigaApplication {
+public class LigaApplication { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(LigaApplication.class, args);
